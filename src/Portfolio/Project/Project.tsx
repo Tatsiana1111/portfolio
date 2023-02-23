80c15232182e44df50a9adff401eb783409f2010
Tatsiana1111 <tanyashket@gmail.com>
2023-02-23 14:21:13 +0300

    react-router-dom

--- src/Portfolio/Project/Project.tsx
+++ src/Portfolio/Project/Project.tsx
@@ -12,8 +12,12 @@ export const Project = (props: ProjectPropsType) => {
         <div className={style.projectBlock}>
             <div className={style.projectBlock}>
                 <div><img className={style.image} src={props.image} alt={props.title}/></div>
-                <span className={style.button}><a href="Tatsiana1111.github.io/todolist"><Button
-                    title={'VIEW PROJECT'}></Button></a></span>
+                <span className={style.button}>
+                    <a rel='noopener noreferrer' href="Tatsiana1111.github.io/todolist">
+                        <Button
+                            title={'VIEW PROJECT'}></Button>
+                    </a>
+                </span>
             </div>
             <h3>{props.title}</h3>
             <span>{props.description}</span>
